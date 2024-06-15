a9da3a61091476d123b4d2f358860669573a96de
Neasa san <nongsuykady2023@gmail.com>
2024-06-15 17:56:17 +0700

    second

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-import { Cardcomponent } from "../component/CardComponent.js";
+
+import { Cardcomponent } from "../components/CardComponent.js";
 import { getData } from "../store/fetchAPI.js";
 
-const renderTocardProduct = document.getElementById("RENDER_PRODUCT");
-    try {
-        const products = await getData("products");
-        console.log("Here product data:", products);
 
-        products.forEach(product => {
-            renderTocardProduct.innerHTML += Cardcomponent(product);
-        });
-    } catch (error) {
-        console.error("Error fetching products:", error);
-    }
+const renderTocardProduct = document.getElementById("RENDER_PRODUCT");
+
+    const products = await getData("products")
+    
+    products.map(product => {
+
+        renderTocardProduct.innerHTML += Cardcomponent(product);
+
+    });
+
+
+    
